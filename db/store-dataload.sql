c356e9700c67a8166b5555911ca5f70be961c806
Erik <oceanuheart@gmail.com>
2021-01-12 11:03:54 +0800

    [feature] 新增歸還功能

--- db/store-dataload.sql
+++ db/store-dataload.sql
@@ -9,4 +9,12 @@ insert into Goods values (8, '拖車', 1700, '', '5A', '');
 insert into Goods values (9, '拖車', 1700, '', '12B', '');
 insert into Goods values (10, '投影筆', 160, '', '12B', '');
 insert into Goods values (11, '投影筆', 170, '', '5A', '');
-
+/*==============================================================*/
+/* Table: records                                                 */
+/*==============================================================*/
+insert into records values (1387,6, 'gansulab', '2020-12-09', '2020-12-11', '未借出');
+insert into records values (13786,9, 'gansulab', '2020-01-10', '', '已借出');
+insert into records values (980,6, 'gansulab', '2020-05-09', '2020-05-11', '未借出');
+insert into records values (8765,9, 'gansulab', '2020-06-18', '', '已借出');
+insert into records values (4321,2, 'gansulab', '2020-01-09', '2020-01-11', '未借出');
+insert into records values (11111,8, 'gansulab', '2020-09-28', '', '已借出');
